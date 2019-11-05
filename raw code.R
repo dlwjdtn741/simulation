8b06437e6957a5c6fd4109df69b2186e0c411de6
Amy Lee <amy.js.lee94@gmail.com>
2019-11-05 16:09:38 -0600

    raw

---
+++ raw code.R
@@ -0,0 +1,31 @@
+lambda=0.2
+
+#thousand simulation, 40 exponentials
+
+#given that lambda is 0.2, the expected mu=5 and sd=5
+
+library(ggplot2)
+
+##for means
+
+exp<-c()
+for (i in 1:1000){
+  exp<-rbind(exp,rexp(40,0.2))
+}
+
+mns<-c()
+for (i in 1:1000) {
+  mns[i]<-mean(exp[i,])
+}
+means<-cumsum(mns[1:1000])/(1:1000) ## cumulative means. how the mean changes as the sample size increases
+
+g_m<-ggplot(data.frame(x=1:1000, y=means), aes(x=x, y=y))+geom_line(size=1.5)
+g_m<-g_m+ylim(min(means)-0.1,max(means)+0.1)+geom_hline(yintercept=5, size=1, color="red")
+g_m<-g_m+labs(title="Change in Sample Mean as Sample Size Increase", x="Sample Size", y="Mean")
+
+means[1000]
+
+##for var
+
+#sample var=sigma^2/n 
+
